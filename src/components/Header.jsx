39c04226d4ec39e91437e01361059e52d64af665
Dona Dutta <donadutta026@gmail.com>
2025-04-20 23:12:07 +0530

    change in code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => (
             alt="Dona Dutta"
             className="w-32 h-32 rounded-full border-4 border-gray-700"
         />
-        <h1 className="text-4xl font-extrabold mt-4 text-white">Dona Dutta Jenkins CI/CD</h1>
+        <h1 className="text-4xl font-extrabold mt-4 text-white">Dona Dutta</h1>
         <p className="text-gray-400 text-lg">Web Developer | Software Engineer | Cloud Enthusiast</p>
         <div className="flex space-x-6 mt-4 text-3xl text-gray-400">
             <a href="https://github.com/dona1503" className="hover:text-teal-400 transition duration-300">
